48216bd9a25c3a039a3a88bd27ad0dd026f74edb
Kevin Kinner <kevin-kinner@web.de>
2024-06-13 12:34:47 +0200

    we have a spinning cube and cards i guess :)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -17,21 +17,39 @@ export default function Dashboard() {
 
 	return (
 		<>
-			<div className="tempWrapper">
-				<label htmlFor="temperature">Current Temperature: </label>
-				{temperature !== null && <span id="temperature">{temperature}°C</span>}
-			</div>
-			<div className="wetWrapper">
-				<label htmlFor="humidity">Current Humidity: </label>
-				{temperature !== null && (
-					<span id="humidity">{randomNumberForTesting()}%</span>
-				)}
-			</div>
-			<div className="cubeWrapper">
-				<label htmlFor="cubeColor">Current Cube Color: </label>
-				{temperature !== null && (
-					<span id="cubeColor">#{randomColorForTesting()}</span>
-				)}
+			<div className="gridWrapper">
+				<div className="cardGrid">
+					<div className="tempWrapper card">
+						<label htmlFor="temperature">Current Temperature: </label>
+						{temperature !== null && (
+							<span id="temperature">{temperature}°C</span>
+						)}
+					</div>
+					<div className="wetWrapper card">
+						<label htmlFor="humidity">Current Humidity: </label>
+						{temperature !== null && (
+							<span id="humidity">{randomNumberForTesting()}%</span>
+						)}
+					</div>
+					<div className="cubeWrapper card">
+						<label htmlFor="cubeColor">Current Cube Color: </label>
+						{temperature !== null && (
+							<span id="cubeColor">#{randomColorForTesting()}</span>
+						)}
+						<span className="cubeSpan">
+							<div className="container">
+								<div className="cube">
+									<div className="face front">#{randomColorForTesting()}</div>
+									<div className="face back">#{randomColorForTesting()}</div>
+									<div className="face right">#{randomColorForTesting()}</div>
+									<div className="face left">#{randomColorForTesting()}</div>
+									<div className="face top">#{randomColorForTesting()}</div>
+									<div className="face bottom">#{randomColorForTesting()}</div>
+								</div>
+							</div>
+						</span>
+					</div>
+				</div>
 			</div>
 			<div className="temperatureChartWrapper">
 				<CardLineChart title="Temperature Development over time" id="1" />
@@ -53,8 +71,12 @@ function randomColorForTesting() {
 }
 
 function changeCubeColorToHex(hexColor: string): void {
-	const cube = document.getElementById("cubeColor");
-	if (cube) {
-		cube.style.backgroundColor = `#${hexColor}`;
+	const cubeFaces: HTMLCollectionOf<Element> =
+		document.getElementsByClassName("face");
+	console.log(cubeFaces);
+
+	//loop through the faces and change the color
+	for (let i = 0; i < cubeFaces.length; i++) {
+		cubeFaces[i].style.backgroundColor = `#${hexColor}`;
 	}
 }
