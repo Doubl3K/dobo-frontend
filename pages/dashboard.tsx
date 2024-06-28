651a50f873cd3745da2eba3d42eacbb90cd7feef
Kevin Kinner <kevin.kinner@css.de>
2024-06-28 13:47:09 +0200

    Data is now displayed in frontend

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,6 +7,7 @@ import "../styles/globals.css";
 
 const GaugeChart = dynamic(() => import("react-gauge-chart"), { ssr: false });
 let client;
+let arrOfEneregyCosts: any = [];
 
 export default function Dashboard() {
 	const [temperature, setTemperature] = useState<number>(0);
@@ -15,19 +16,18 @@ export default function Dashboard() {
 	const [mqttDataRecieved, setMqttDataRecieved] = useState<string>(
 		"No data recieved yet"
 	);
+	const [apiData, setApiData] = useState<string>("No data recieved yet");
 	const [mqttData, setMqttData] = useState<string[]>([]); // New state variable
 
 	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setTemperature(randomNumberForTesting());
-			setHumidity(randomNumberForTesting());
-			const newColor = randomColorForTesting();
-			setCubeColor(newColor);
-			changeCubeColorToHex(newColor);
-			setMqttDataRecieved(randomMqttData());
-			const randomData = randomMqttData();
-			connectMe(setMqttData);
-		}, 1000);
+		setMqttDataRecieved(readJsonForTesting(JsonMockup));
+		setTemperature(getTempTemp());
+		setHumidity(getTempHumidity());
+		setCubeColor(getTempColor());
+		changeCubeColorToHex(getTempColor());
+		setApiData(getTempCosts());
+		setMqttDataRecieved;
+		const intervalId = setInterval(() => {}, 1000);
 
 		// cleanup function to clear the interval when the component unmounts
 		return () => clearInterval(intervalId);
@@ -40,7 +40,6 @@ export default function Dashboard() {
 				<div className="cardGrid">
 					<div className="tempWrapper card">
 						<label htmlFor="temperature">Current Temperature: </label>
-						mqtt
 						<GaugeChart
 							id="gauge-chart1"
 							percent={temperature / 100}
@@ -58,16 +57,16 @@ export default function Dashboard() {
 					</div>
 					<div className="cubeWrapper card">
 						<label htmlFor="cubeColor">Current Cube Color: </label>
-						{temperature !== null && <span id="cubeColor">#{cubeColor}</span>}
+						{temperature !== null && <span id="cubeColor">{cubeColor}</span>}
 						<span className="cubeSpan">
 							<div className="container">
 								<div className="cube">
-									<div className="face front">#{cubeColor}</div>
-									<div className="face back">#{cubeColor}</div>
-									<div className="face right">#{cubeColor}</div>
-									<div className="face left">#{cubeColor}</div>
-									<div className="face top">#{cubeColor}</div>
-									<div className="face bottom">#{cubeColor}</div>
+									<div className="face front">{cubeColor}</div>
+									<div className="face back">{cubeColor}</div>
+									<div className="face right">{cubeColor}</div>
+									<div className="face left">{cubeColor}</div>
+									<div className="face top">{cubeColor}</div>
+									<div className="face bottom">{cubeColor}</div>
 								</div>
 							</div>
 						</span>
@@ -78,7 +77,7 @@ export default function Dashboard() {
 					</div>
 					<div className="dataRecieved card">
 						<label htmlFor="data">Cost table</label>
-						<p id="data">No data recieved yet</p>
+						<p id="data">{createApiTable(arrOfEneregyCosts)}</p>
 						<button
 							onClick={() => connectMe(setMqttData)}
 							className="connectButton">
@@ -86,12 +85,8 @@ export default function Dashboard() {
 						</button>
 					</div>
 					<div className="dateRecievedHistory card">
-						<label htmlFor="mqttData">MQTT data: </label>
-						<span className="dataHistory mqtt">
-							{mqttData.length === 0
-								? "No Data received yet"
-								: mqttData.join(", ")}
-						</span>
+						<label htmlFor="mqttData">MQTT data:</label>
+						<span className="dataHistory mqtt">{mqttDataRecieved}</span>
 						<button onClick={() => sendTestData()} className="connectButton">
 							Connect
 						</button>
@@ -102,21 +97,21 @@ export default function Dashboard() {
 	);
 }
 
-function randomNumberForTesting() {
-	return Math.floor(Math.random() * 100);
-}
+// function randomNumberForTesting() {
+// 	return Math.floor(Math.random() * 100);
+// }
 
-function randomColorForTesting() {
-	const hexColor = Math.floor(Math.random() * 16777215).toString(16);
-	return hexColor.padStart(6, "0");
-}
+// function randomColorForTesting() {
+// 	const hexColor = Math.floor(Math.random() * 16777215).toString(16);
+// 	return hexColor.padStart(6, "0");
+// }
 
 function changeCubeColorToHex(hexColor: string): void {
 	const cubeFaces: HTMLCollectionOf<Element> =
 		document.getElementsByClassName("face");
 
 	for (let i = 0; i < cubeFaces.length; i++) {
-		cubeFaces[i].style.backgroundColor = `#${hexColor}`;
+		cubeFaces[i].style.backgroundColor = `${hexColor}`;
 	}
 }
 
@@ -126,22 +121,20 @@ function randomMqttData() {
 	return data + random;
 }
 
-function connectMe(
-	setMqttData: React.Dispatch<React.SetStateAction<string[]>>
-) {
-	document.getElementsByClassName("dataHistory")[0]!.innerHTML =
-		"Connecting To MQTT Server...";
+// function connectMe(
+// 	setMqttData: React.Dispatch<React.SetStateAction<string[]>>
+// ) {
+// 	document.getElementsByClassName("dataHistory")[0]!.innerHTML =
+// 		"Connecting To MQTT Server...";
 
-	setTimeout(() => {
-		let test = document.createElement("p");
-		const randomData = randomMqttData();
-		setMqttData((prevData) => [...prevData, randomData]);
-		const dataHistoryElement =
-			document.getElementsByClassName("dataHistory")[0];
-		test.textContent = randomData;
-		dataHistoryElement.appendChild(test);
-	}, 2000);
-}
+// 	setTimeout(() => {
+// 		let test = document.createElement("p");
+// 		const randomData = randomMqttData();
+// 		setMqttData((prevData) => [...prevData, randomData]);
+// 		test.textContent = randomData;
+// 	}, 2000);
+// }
+
 function sendTestData() {
 	let data: JSON = JsonMockup;
 	console.log(data.toString());
@@ -150,16 +143,13 @@ function sendTestData() {
 	console.log(JsonMockup);
 }
 
-let official = "FBS/LF8_Projekt/JSON";
-let test = "jsonTest";
-
-function startMqttConnection() {
+function startMqttConnection(topic: string) {
 	console.log("Starting MQTT Connection");
 
 	client = mqtt.connect("ws://test.mosquitto.org:8080");
 
 	client.on("connect", () => {
-		client.subscribe(test, (err) => {
+		client.subscribe(topic, (err) => {
 			if (!err) {
 				console.log(test);
 			}
@@ -182,4 +172,179 @@ function getEnergieCosts(data: JSON) {
 	let costs = data[0];
 	console.log(costs);
 }
-startMqttConnection();
+//Change test to offical in startMQTTconnection when presenting
+let official = "FBS/LF8_Projekt/JSON";
+let test = "jsonTest";
+// startMqttConnection(test);
+
+function readJsonForTesting(data: any) {
+	data = JsonMockup;
+	let humidity;
+	let temperature;
+	let color;
+	let costs;
+
+	if (data) {
+		humidity = getJsonObjectByKey(data, "Feuchtigkeit");
+		temperature = getJsonObjectByKey(data, "Temperatur");
+		color = getJsonObjectByKey(data, "Farbwerte");
+		costs = getJsonObjectByKey(data, "Energiekosten");
+	}
+
+	let humidityArrayFirstElement;
+	let temperatureArrayFirstElement;
+	let colorArrayFirstElement;
+
+	if (humidity) {
+		humidityArrayFirstElement = getJsonArrayFirstElement(humidity);
+	}
+
+	if (temperature) {
+		temperatureArrayFirstElement = getJsonArrayFirstElement(temperature);
+	}
+
+	if (color) {
+		colorArrayFirstElement = getJsonArrayFirstElement(color);
+	}
+
+	if (costs) {
+		getAllCosts(costs);
+	}
+
+	let humidityResult;
+	let temperatureResult;
+	let colorResult;
+
+	if (humidityArrayFirstElement) {
+		humidityResult = getJsonObjectByKey(humidityArrayFirstElement, "humidity");
+	}
+
+	if (temperatureArrayFirstElement) {
+		temperatureResult = getJsonObjectByKey(
+			temperatureArrayFirstElement,
+			"temperature"
+		);
+	}
+
+	if (colorArrayFirstElement) {
+		colorResult = getJsonObjectByKey(colorArrayFirstElement, "hex_code");
+	}
+
+	let apiTable = createApiTable(arrOfEneregyCosts);
+
+	setTempTemp(temperatureResult);
+	setTempHumidity(humidityResult);
+	setTempColor(colorResult);
+	setmqData(JsonMockup);
+	setTempCosts(apiTable);
+	return JSON.stringify(data);
+}
+
+function getJsonObjectByKey(data: any, key: string) {
+	return data[key];
+}
+
+function getJsonArrayFirstElement(array: any) {
+	if (array) {
+		return array[0];
+	}
+}
+
+function getAllCosts(array: any) {
+	array.forEach((element: string) => {
+		let startTimeDate;
+		let endTimeDate;
+		let startTime;
+		let endTime;
+		let costs;
+		try {
+			startTimeDate = getJsonObjectByKey(element, "Start");
+			startTime = getJsonObjectByKey(startTimeDate, "$date");
+			endTimeDate = getJsonObjectByKey(element, "End");
+			endTime = getJsonObjectByKey(endTimeDate, "$date");
+			costs = getJsonObjectByKey(element, "Marketprice");
+		} catch (error) {
+			console.log("Error in parsing JSON");
+		}
+		arrOfEneregyCosts.push(startTime);
+		arrOfEneregyCosts.push(endTime);
+		arrOfEneregyCosts.push(costs);
+	});
+}
+
+/* Put data into frontend
+Make table from costs.
+*/
+let tempTemp;
+let tempHumidity;
+let tempColor;
+let mqData;
+let tempCosts;
+
+function setTempTemp(temp: number) {
+	tempTemp = temp;
+}
+
+function getTempTemp() {
+	console.log(tempTemp);
+
+	return tempTemp;
+}
+
+function setTempHumidity(humidity: number) {
+	tempHumidity = humidity;
+}
+
+function getTempHumidity() {
+	return tempHumidity;
+}
+
+function setTempColor(color: string) {
+	tempColor = color;
+}
+
+function getTempColor() {
+	return tempColor;
+}
+
+function setmqData(data: string) {
+	mqData = data;
+}
+
+function getmqData() {
+	return mqData;
+}
+
+function setTempCosts(costs: any) {
+	tempCosts = costs;
+}
+
+function getTempCosts() {
+	return tempCosts;
+}
+
+function createApiTable(array: any) {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Start</th>
+					<th>End</th>
+					<th>Costs</th>
+				</tr>
+			</thead>
+			<tbody>
+				{array.map(
+					(item: any, index: number) =>
+						index % 3 === 0 && (
+							<tr key={index}>
+								<td>{array[index]}</td>
+								<td>{array[index + 1]}</td>
+								<td>{array[index + 2]}</td>
+							</tr>
+						)
+				)}
+			</tbody>
+		</table>
+	);
+}
