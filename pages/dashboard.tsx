52a2de89f98da60b95042bbc3fead8335c6b21cd
Kevin Kinner <kevin-kinner@web.de>
2024-06-29 15:49:53 +0200

    Final testing cleanup

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,24 @@
 import mqtt from "mqtt";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import JsonMockup from "../docs/mockupData.json";
 import CardLineChart from "../src/components/Chart";
 import "../styles/globals.css";
 
 const GaugeChart = dynamic(() => import("react-gauge-chart"), { ssr: false });
 let client;
 let arrOfEneregyCosts: any = [];
+let dataRecive = false;
+let official = "FBS/LF8_Projekt/JSON";
 
+/**
+ * This is the main dashboard View. It contains all the other components and is the main component of the application.
+ * It uses the useState and useEffect hooks to manage the state of the application.
+ * @returns The dashboard component
+ */
 export default function Dashboard() {
 	const [temperature, setTemperature] = useState<number>(0);
 	const [humidity, setHumidity] = useState<number>(0);
-	const [cubeColor, setCubeColor] = useState<string>("000000");
+	const [cubeColor, setCubeColor] = useState<string>("#000000");
 	const [mqttDataRecieved, setMqttDataRecieved] = useState<string>(
 		"No data recieved yet"
 	);
@@ -20,14 +26,22 @@ export default function Dashboard() {
 	const [mqttData, setMqttData] = useState<string[]>([]); // New state variable
 
 	useEffect(() => {
-		setMqttDataRecieved(handleJson(JsonMockup));
-		setTemperature(getTempTemp());
-		setHumidity(getTempHumidity());
-		setCubeColor(getTempColor());
-		changeCubeColorToHex(getTempColor());
+		setMqttDataRecieved("No data received yet");
+		setTemperature(0);
+		setHumidity(0);
+		setCubeColor("#000000");
+		changeCubeColorToHex("#000000");
 		setApiData(getTempCosts());
-		setMqttDataRecieved;
-		const intervalId = setInterval(() => {}, 1000);
+		const intervalId = setInterval(() => {
+			if (dataRecive === true) {
+				setMqttDataRecieved(JSON.stringify(getmqData()));
+				setTemperature(getTempTemp());
+				setHumidity(getTempHumidity());
+				setCubeColor(getTempColor());
+				changeCubeColorToHex(getTempColor());
+				setApiData(getTempCosts());
+			}
+		}, 1000);
 
 		// cleanup function to clear the interval when the component unmounts
 		return () => clearInterval(intervalId);
@@ -76,12 +90,16 @@ export default function Dashboard() {
 					</div>
 					<div className="dataRecieved card">
 						<label htmlFor="data">Cost table</label>
-						<div id="data">{createApiTable(arrOfEneregyCosts)}</div>
+						<div id="data" className="apiDataDiv">
+							{createApiTable(arrOfEneregyCosts)}
+						</div>
 					</div>
 					<div className="dateRecievedHistory card">
 						<label htmlFor="mqttData">MQTT data:</label>
 						<span className="dataHistory mqtt">{mqttDataRecieved}</span>
-						<button onClick={() => sendTestData()} className="connectButton">
+						<button
+							onClick={() => startMqttConnection()}
+							className="connectButton">
 							Reconnect
 						</button>
 					</div>
@@ -91,6 +109,10 @@ export default function Dashboard() {
 	);
 }
 
+/**
+ * Changes the color of the cube to the given hex color.
+ * @param hexColor The hex color to change the cube to
+ */
 function changeCubeColorToHex(hexColor: string): void {
 	const cubeFaces: HTMLCollectionOf<Element> =
 		document.getElementsByClassName("face");
@@ -100,20 +122,19 @@ function changeCubeColorToHex(hexColor: string): void {
 	}
 }
 
-function sendTestData() {
-	let data: JSON = JsonMockup;
-
-	client.publish("jsonTest", JSON.stringify(data));
-}
-
-function startMqttConnection(topic: string) {
+/**
+ * Starts the MQTT connection and subscribes to the given topic.
+ */
+function startMqttConnection() {
+	let topic = official;
 	console.log("Starting MQTT Connection");
-
 	client = mqtt.connect("ws://test.mosquitto.org:8080");
 
 	client.on("connect", () => {
+		console.log("Connected to MQTT");
 		client.subscribe(topic, (err) => {
-			if (!err) {
+			console.log("Subscribed to: " + topic);
+			if (err) {
 				console.error(err);
 			}
 		});
@@ -121,17 +142,18 @@ function startMqttConnection(topic: string) {
 
 	client.on("message", (topic, message) => {
 		console.log("Data recieved");
+		dataRecive = true;
 		handleJson(message);
 	});
 }
 
-//Change test to offical in startMQTTconnection when presenting
-let official = "FBS/LF8_Projekt/JSON";
-let test = "jsonTest";
-// startMqttConnection(test);
-
+/**
+ * Handles the JSON data recieved from the MQTT connection.
+ * @param data The JSON data recieved from the MQTT connection
+ * @returns The JSON data as a string
+ */
 function handleJson(data: any) {
-	data = JsonMockup;
+	data = JSON.parse(data);
 	let humidity;
 	let temperature;
 	let color;
@@ -144,50 +166,29 @@ function handleJson(data: any) {
 		costs = getJsonObjectByKey(data, "Energiekosten");
 	}
 
-	let humidityArrayFirstElement;
-	let temperatureArrayFirstElement;
-	let colorArrayFirstElement;
-
-	if (humidity) {
-		humidityArrayFirstElement = getJsonArrayFirstElement(humidity);
-	}
-
-	if (temperature) {
-		temperatureArrayFirstElement = getJsonArrayFirstElement(temperature);
-	}
-
-	if (color) {
-		colorArrayFirstElement = getJsonArrayFirstElement(color);
-	}
-
-	if (costs) {
-		getAllCosts(costs);
-	}
-
 	let humidityResult;
 	let temperatureResult;
 	let colorResult;
 
-	if (humidityArrayFirstElement) {
-		humidityResult = getJsonObjectByKey(humidityArrayFirstElement, "humidity");
+	if (humidity) {
+		humidityResult = getJsonObjectByKey(humidity, "humidity");
 	}
 
-	if (temperatureArrayFirstElement) {
-		temperatureResult = getJsonObjectByKey(
-			temperatureArrayFirstElement,
-			"temperature"
-		);
+	if (temperature) {
+		temperatureResult = getJsonObjectByKey(temperature, "temperature");
 	}
 
-	if (colorArrayFirstElement) {
-		colorResult = getJsonObjectByKey(colorArrayFirstElement, "hex_code");
+	if (color) {
+		colorResult = getJsonObjectByKey(color, "hex_code");
 	}
 	let apiTable;
 
 	setTempTemp(temperatureResult);
 	setTempHumidity(humidityResult);
 	setTempColor(colorResult);
-	setmqData(JsonMockup);
+	setmqData(data);
+	getAllCosts(costs);
+	console.log(arrOfEneregyCosts);
 
 	if (arrOfEneregyCosts.length != 0) {
 		apiTable = createApiTable(arrOfEneregyCosts);
@@ -198,18 +199,35 @@ function handleJson(data: any) {
 	return JSON.stringify(data);
 }
 
+/**
+ * Gets the JSON object by the given key.
+ * @param data The JSON data to get the object from
+ * @param key The key to get the object from
+ * @returns The JSON object
+ */
 function getJsonObjectByKey(data: any, key: string) {
 	return data[key];
 }
 
+/**
+ * Gets the first element of a JSON array.
+ * @param array The JSON array to get the first element from
+ * @returns The first element of the JSON array
+ */
 function getJsonArrayFirstElement(array: any) {
 	if (array) {
 		return array[0];
 	}
 }
 
+/**
+ * Gets all the costs from the JSON array.
+ * @param array The JSON array to get the costs from
+ */
 function getAllCosts(array: any) {
 	if (array.length === 0) {
+		console.log("No costs found");
+
 		return;
 	}
 	array.forEach((element: string) => {
@@ -218,12 +236,14 @@ function getAllCosts(array: any) {
 		let startTime;
 		let endTime;
 		let costs;
+		console.log(array);
+
 		try {
-			startTimeDate = getJsonObjectByKey(element, "Start");
+			startTimeDate = getJsonObjectByKey(element, "start");
 			startTime = getJsonObjectByKey(startTimeDate, "$date");
-			endTimeDate = getJsonObjectByKey(element, "End");
+			endTimeDate = getJsonObjectByKey(element, "end");
 			endTime = getJsonObjectByKey(endTimeDate, "$date");
-			costs = getJsonObjectByKey(element, "Marketprice");
+			costs = getJsonObjectByKey(element, "marketprice");
 		} catch (error) {
 			console.error("Error in parsing JSON");
 		}
@@ -233,55 +253,98 @@ function getAllCosts(array: any) {
 	});
 }
 
-/* Put data into frontend
-Make table from costs.
-*/
 let tempTemp;
 let tempHumidity;
 let tempColor;
 let mqData;
 let tempCosts;
 
+/**
+ * Sets the temperature to the given value.
+ * @param temp The temperature to set
+ * @returns The temperature
+ */
 function setTempTemp(temp: number) {
 	tempTemp = temp;
 }
 
+/**
+ * Gets the temperature.
+ * @returns The temperature
+ */
 function getTempTemp() {
 	return tempTemp;
 }
 
+/**
+ * Sets the humidity to the given value.
+ * @param humidity The humidity to set
+ */
 function setTempHumidity(humidity: number) {
 	tempHumidity = humidity;
 }
 
+/**
+ * Gets the humidity.
+ * @returns The humidity
+ */
 function getTempHumidity() {
 	return tempHumidity;
 }
 
+/**
+ * Sets the color to the given value.
+ * @param color The color to set
+ */
 function setTempColor(color: string) {
 	tempColor = color;
 }
 
+/**
+ * Gets the color.
+ * @returns The color
+ */
 function getTempColor() {
 	return tempColor;
 }
 
+/**
+ * Sets the MQTT data to the given value.
+ * @param data The data to set
+ */
 function setmqData(data: string) {
 	mqData = data;
 }
 
+/**
+ * Gets the MQTT data.
+ * @returns The MQTT data
+ */
 function getmqData() {
 	return mqData;
 }
 
+/**
+ * Sets the costs to the given value.
+ * @param costs The costs to set
+ */
 function setTempCosts(costs: any) {
 	tempCosts = costs;
 }
 
+/**
+ * Gets the costs.
+ * @returns The costs
+ */
 function getTempCosts() {
 	return tempCosts;
 }
 
+/**
+ * Creates a table with the given array.
+ * @param array The array to create the table from
+ * @returns The table
+ */
 function createApiTable(array: any) {
 	return (
 		<table className="apiTable">
@@ -307,3 +370,6 @@ function createApiTable(array: any) {
 		</table>
 	);
 }
+
+// Start the MQTT connection on page load
+startMqttConnection();
